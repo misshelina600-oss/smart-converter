8f020549b0b46a51d47b95f50a2ec35239232b70
misshelina600-oss <misshelina600@gmail.com>
2026-07-23 13:42:49 +0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const upload = multer({ dest: 'uploads/' });
 
 app.get('/', (req, res) => {
-    res.send('Smart Converter Server is Running Successfully!');
+    res.send('Smart Converter Server is Running!');
 });
 
 app.post('/convert', upload.single('file'), (req, res) => {
@@ -20,17 +20,19 @@ app.post('/convert', upload.single('file'), (req, res) => {
     const originalName = path.parse(req.file.originalname).name;
     const outputPath = path.join('uploads', `${originalName}-${Date.now()}.pdf`);
 
-    const fileExt = path.extname(req.file.originalname);
-
     fs.readFile(inputPath, (err, fileData) => {
         if (err) {
             return res.status(500).send('Error reading uploaded file.');
         }
 
+        // LibreOffice কনভার্শন
         libre.convert(fileData, '.pdf', undefined, (err, done) => {
+            // ফাইল আপলোড ইনপুট রিমুভ করে দেই
+            fs.unlink(inputPath, () => {});
+
             if (err) {
                 console.error('Conversion error:', err);
-                return res.status(500).send('Error converting file: ' + err.message);
+                return res.status(500).send('Conversion error: ' + err.message);
             }
 
             fs.writeFile(outputPath, done, (err) => {
@@ -39,8 +41,7 @@ app.post('/convert', upload.single('file'), (req, res) => {
                 }
 
                 res.download(outputPath, `${originalName}.pdf`, (err) => {
-                    // ক্লিনআপ ফাইলস
-                    fs.unlink(inputPath, () => {});
+                    // ডাউনলোড শেষ হলে সার্ভার থেকে ফাইল ডিলিট করে দেব
                     fs.unlink(outputPath, () => {});
                 });
             });
